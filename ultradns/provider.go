c706111e54fb072d3c3dbce8039c52d4dafe0603
DJ <davidji99@users.noreply.github.com>
2021-02-25 17:07:34 +0900

    upgrade tf sdk to v2

--- ultradns/provider.go
+++ ultradns/provider.go
@@ -1,8 +1,8 @@
 package ultradns
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"github.com/terra-farm/udnssdk"
 )
 

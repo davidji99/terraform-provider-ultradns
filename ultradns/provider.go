a15cd6a054d1d11af18a5e418566e0712249a835
DJ <davidji99@users.noreply.github.com>
2021-02-25 17:12:21 +0900

    Upgrade tf sdk to v2 (#3)
    * remove depreciated hashcodestring
    
    * update go.sum
    
    * remove more deprecated hashcode pkg
    
    * upgrade tf sdk to v2
    
    * fix provider initialization

--- ultradns/provider.go
+++ ultradns/provider.go
@@ -1,13 +1,12 @@
 package ultradns
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/terra-farm/udnssdk"
 )
 
 // Provider returns a terraform.ResourceProvider.
-func Provider() terraform.ResourceProvider {
+func Provider() *schema.Provider {
 	return &schema.Provider{
 		Schema: map[string]*schema.Schema{
 			"username": {
